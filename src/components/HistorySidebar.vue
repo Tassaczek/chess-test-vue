4306c29af6b2986eed04136b13d6e8f16ce4cea7
Tassak <damian@mythe.pl>
2024-04-08 22:57:10 +0200

    Add History Sidebar with basic styling

---
+++ src/components/HistorySidebar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="history-sidebar">
+		<div class="history-sidebar__navigation">
+			<button>prev</button>
+			<button>next</button>
+		</div>
+
+		<ol>
+			<li>a8</li>
+			<li>a8</li>
+			<li>a8</li>
+			<li>a8</li>
+		</ol>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.history-sidebar {
+	border-radius: 5px;
+	width: 100%;
+	background-color: rgb(249, 236, 211);
+	&__navigation {
+		display: flex;
+		justify-content: center;
+	}
+}
+
+@media (min-width: 960px) {
+	.history-sidebar {
+		flex-grow: 1;
+		max-width: 200px;
+	}
+}
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
 import TheChessboard from '../components/TheChessboard.vue';
+import HistorySidebar from '../components/HistorySidebar.vue';
 </script>
 
 <template>
 	<div class="game-view">
 		<TheChessboard />
+		<HistorySidebar />
 	</div>
 </template>
 
 <style scoped lang="scss">
 .game-view {
-	min-height: 100vh;
 	display: flex;
-	justify-content: center;
+	flex-direction: column;
 	align-items: center;
+	margin-top: 2rem;
+	margin-right: 2rem;
+	margin-left: 2rem;
+	gap: 2rem;
+}
+
+@media (min-width: 960px) {
+	.game-view {
+		flex-direction: row;
+		justify-content: center;
+		gap: 2rem;
+		align-items: stretch;
+	}
 }
 </style>
