e6be9658e32b3431820e043ecf636bd2a43bdae9
Tassak <damian@mythe.pl>
2024-04-10 01:15:54 +0200

    Add structure to sidebar

--- src/components/HistorySidebar.vue
+++ src/components/HistorySidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import BaseButton from './BaseButton.vue';
 interface Props {
 	history: string[];
 	activeSquareIndex: number;
@@ -12,37 +13,69 @@ const emit = defineEmits<{
 
 <template>
 	<div class="history-sidebar">
-		<div class="history-sidebar__navigation">
-			<button>prev</button>
-			<button>next</button>
-			<button @click="emit('clearHistory')">Reset</button>
-		</div>
-
-		<ol>
-			<li
-				v-for="position in props.history"
-				:key="position"
+		<section class="history-sidebar__reset">
+			<BaseButton
+				aria-label="Reset"
+				@click="emit('clearHistory')"
+				>RESET</BaseButton
 			>
-				{{ position }}
-			</li>
-		</ol>
+		</section>
+		<section class="history-sidebar__main">
+			<ol>
+				<li
+					v-for="position in props.history"
+					:key="position"
+				>
+					{{ position }}
+				</li>
+			</ol>
+		</section>
+		<section class="history-sidebar__navigation">
+			<BaseButton
+				aria-label="Reset"
+				@click="emit('clearHistory')"
+				>Navigation here</BaseButton
+			>
+		</section>
 	</div>
 </template>
 
 <style scoped lang="scss">
 .history-sidebar {
-	border-radius: 5px;
 	width: 100%;
-	background-color: var(--primary);
-	&__navigation {
+	max-height: calc(100vh - 4rem);
+	&__reset {
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
+		padding: 1rem;
+		background-color: var(--neutral-1);
 		display: flex;
 		justify-content: center;
+	}
+	&__main {
+		background-color: var(--primary);
+		overflow-y: auto;
+		height: 350px;
+	}
+	&__navigation {
+		border-bottom-left-radius: 5px;
+		border-bottom-right-radius: 5px;
+		background-color: var(--neutral-1);
+		display: flex;
+		justify-content: center;
+		padding: 1rem;
 	}
 }
 
 @media (min-width: 960px) {
 	.history-sidebar {
 		max-width: 200px;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		&__main {
+			flex-grow: 1;
+		}
 	}
 }
 </style>
